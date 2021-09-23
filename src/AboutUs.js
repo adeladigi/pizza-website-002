02d505167508572b4ca373425083bded57cf81dc
Marcos <greendogpop@gmail.com>
2021-09-23 00:49:53 -0400

    changed some info in about us page

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -11,15 +11,15 @@ function AboutUs(){
             </div>
 
 
-           <p>WelcomeIn  1985,  Chef  Ernesto  opened  A&V  Pizzeria and   Family   Restaurant.
-              Today,   under   the care  of  Pino  &  Gina,  A&V  continues  to  be part     of 
+           <p>WelcomeIn  1985,  Chef  Ernesto  opened  Adela's Pizza and   Family   Restaurant.
+              Today,   under   the care  of  jim  &  kat,  k&V  continues  to  be part     of 
               the     community,     with     many customers  having  been  raised  on  our  food.
-              A&V   is   a   great   place   for   a   quick   bite   or dinner   with   the   whole
+              k&V   is   a   great   place   for   a   quick   bite   or dinner   with   the   whole
               family.   Order   for pickup     or     delivery,     eat     in     our     cozy restaurant,
-              or   celebrate   special   occasions with   a   private   party.   At   A&V,   there   is   a
+              or   celebrate   special   occasions with   a   private   party.   At   k&V,   there   is   a
               commitment  to  using  quality  ingredients and    serving    fresh    food,    with    generous
-              portions.   The   staff   at   A&V   goes   the   extra mile   to   ensure   the   best   service
-              possible, and  you  are  treated  like  a  friend.  A&V  is  a small,  old  school  neighborhood
+              portions.   The   staff   at   k&V   goes   the   extra mile   to   ensure   the   best   service
+              possible, and  you  are  treated  like  a  friend.  k&V  is  a small,  old  school  neighborhood
               place.  The restaurant  is  designed  to  make  you  feel  like you  are  sitting  and  relaxing
               in  a  outdoor Italian courtyard.</p>
         </div>
